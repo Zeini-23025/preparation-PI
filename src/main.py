5a0d9cb22eda82b7707a0b3839fec526c73c592b
zeini <23025@supnum.mr>
2025-05-15 13:25:07 +0000

    ajouter des examples et README

--- src/main.py
+++ src/main.py
@@ -51,12 +51,81 @@ def draw_gantt_chart(tasks):
     plt.tight_layout()
     plt.show()
 
-# Exemple
+# Exemples
 tasks = [
     Task("A1", 3, 2, 5),
     Task("A2", 2, 3, 4, ["A1"]),
     Task("A3", 4, 2, 3, ["A1"]),
 ]
+
+
+tasks4 = [
+    Task("T1", 2, 2, 10),
+    Task("T2", 1, 1, 5),
+    Task("T3", 3, 2, 8, ["T1"]),
+    Task("T4", 2, 2, 6, ["T1"]),
+    Task("T5", 4, 1, 7, ["T2", "T3"]),
+    Task("T6", 2, 2, 4, ["T4"]),
+    Task("T7", 1, 1, 3, ["T5", "T6"]),
+]
+
+tasks20 = [
+    Task("T1", 2, 1, 10),
+    Task("T2", 3, 2, 8),
+    Task("T3", 1, 1, 6, ["T1"]),
+    Task("T4", 2, 2, 7, ["T1"]),
+    Task("T5", 4, 2, 5, ["T2"]),
+    Task("T6", 1, 1, 9, ["T3", "T4"]),
+    Task("T7", 2, 3, 6, ["T5"]),
+    Task("T8", 3, 2, 4),
+    Task("T9", 1, 1, 7, ["T6"]),
+    Task("T10", 2, 1, 6, ["T6"]),
+    Task("T11", 2, 2, 5, ["T7"]),
+    Task("T12", 3, 1, 4),
+    Task("T13", 2, 2, 5, ["T11"]),
+    Task("T14", 1, 1, 6, ["T12"]),
+    Task("T15", 2, 2, 8, ["T13", "T14"]),
+    Task("T16", 3, 3, 6),
+    Task("T17", 1, 1, 7, ["T9", "T10"]),
+    Task("T18", 2, 2, 6, ["T15"]),
+    Task("T19", 3, 2, 5, ["T17"]),
+    Task("T20", 1, 1, 9, ["T18", "T19"]),
+]
+
+tasks30 = [
+    Task("T1", 2, 2, 9),
+    Task("T2", 3, 1, 8),
+    Task("T3", 1, 1, 7),
+    Task("T4", 2, 2, 6, ["T1"]),
+    Task("T5", 3, 1, 5, ["T1", "T2"]),
+    Task("T6", 2, 2, 4, ["T2"]),
+    Task("T7", 1, 1, 3, ["T3"]),
+    Task("T8", 3, 2, 6, ["T4"]),
+    Task("T9", 2, 3, 5, ["T5"]),
+    Task("T10", 1, 1, 4, ["T6"]),
+    Task("T11", 2, 2, 3, ["T7"]),
+    Task("T12", 3, 1, 6, ["T8"]),
+    Task("T13", 2, 2, 5, ["T9"]),
+    Task("T14", 1, 1, 4, ["T10"]),
+    Task("T15", 2, 3, 3, ["T11"]),
+    Task("T16", 3, 2, 6, ["T12"]),
+    Task("T17", 1, 1, 5, ["T13"]),
+    Task("T18", 2, 1, 4, ["T14", "T15"]),
+    Task("T19", 1, 2, 3, ["T16"]),
+    Task("T20", 2, 1, 6, ["T17"]),
+    Task("T21", 3, 2, 5, ["T18"]),
+    Task("T22", 1, 1, 4, ["T19"]),
+    Task("T23", 2, 2, 3, ["T20"]),
+    Task("T24", 1, 1, 2, ["T21"]),
+    Task("T25", 2, 2, 6, ["T22"]),
+    Task("T26", 1, 1, 5, ["T23"]),
+    Task("T27", 2, 2, 4, ["T24"]),
+    Task("T28", 1, 1, 3, ["T25"]),
+    Task("T29", 2, 3, 2, ["T26", "T27"]),
+    Task("T30", 3, 2, 1, ["T28", "T29"]),
+]
+
+
 
 scheduled, makespan, cost = schedule_tasks(tasks, max_resource=4)
 
@@ -66,3 +135,4 @@ for task in scheduled:
     print(f"{task.name} : {task.start_time} → {task.end_time}")
 
 draw_gantt_chart(scheduled)
+
